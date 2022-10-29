d67832633cc755a3c8a93052ce7e367961c5b96c
Purusottam <mpurusottamc@gmail.com>
2022-10-29 14:57:12 -0600

    version upgrade to 1.0.17

--- cloudconsolelink/__init__.py
+++ cloudconsolelink/__init__.py
@@ -1,2 +1,2 @@
 # version of the module
-__version__ = '1.0.16'
+__version__ = '1.0.17'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md") as fh:
     long_description = fh.read()
 
 # version of the module
-__version__ = '1.0.16'
+__version__ = '1.0.17'
 
 
 setuptools.setup(
