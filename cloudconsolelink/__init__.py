d2b367620ebe3b2d7528e5bbfd1c3c6245978e4f
Purusottam <mpurusottamc@gmail.com>
2024-07-12 17:56:57 -0700

    aws internet gateway support added

--- cloudconsolelink/__init__.py
+++ cloudconsolelink/__init__.py
@@ -1,2 +1,2 @@
 # version of the module
-__version__ = '1.0.23'
+__version__ = '1.0.24'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md") as fh:
     long_description = fh.read()
 
 # version of the module
-__version__ = '1.0.23'
+__version__ = '1.0.24'
 
 
 setuptools.setup(
